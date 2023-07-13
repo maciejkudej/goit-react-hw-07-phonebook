7d6a5ac66b9a719849d9febca7104e3517f29c2c
maciejkudej <maciejkudej765@gmail.com>
2023-07-13 18:07:02 +0200

    Add task7

---
+++ src/redux/contactsOperation.js
@@ -0,0 +1,23 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import * as api from '../utils/api';
+
+export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
+  const contacts = await api.fetchContacts();
+  return contacts;
+});
+
+export const deleteContacts = createAsyncThunk(
+  'contacts/deleteContact',
+  async id => {
+    const data = await api.deleteContacts(id);
+    return data;
+  }
+);
+
+export const addContacts = createAsyncThunk(
+  'contacts/addContact',
+  async contact => {
+    const data = await api.addContacts(contact);
+    return data;
+  }
+);
